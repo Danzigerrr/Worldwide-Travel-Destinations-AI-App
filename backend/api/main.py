aa27172315fe5fbf2e2341e950e23e3dbf9a025a
Krzysztof Nazar <s184698@student.pg.edu.pl>
2025-07-21 20:29:48 +0200

    refactor code (clean)

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
-from chat_utils import ChatHandler, UserMessage
-
+from backend.api.chat.chat_utils import ChatHandler, UserMessage
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
 chat_handler = ChatHandler()
+
+@app.get("/health_check/")
+def health_check():
+    return "Health check complete - the app is working!"
 
 @app.post("/chat/")
 async def chat(new_message: UserMessage):
